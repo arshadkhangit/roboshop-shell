c62e3587b1e3deb73b2bcad38bdd958327db3c2a
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-25 21:31:54 +0530

    mongodb scripit

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,9 @@
 echo -e "\e[33mcop mongodb repo\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/robodhop.log
 echo -e "\e[33minstalling mongodb server\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/robodhop.log
 
 #modify the config file
 echo -e "\e[33mstarting mongodb server\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/robodhop.log
+systemctl restart mongod &>>/tmp/robodhop.log

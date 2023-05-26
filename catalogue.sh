0b33a1669427a1532c93117a8b629efd4cc62b14
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-26 17:08:25 +0530

    Redis Script

--- catalogue.sh
+++ catalogue.sh
@@ -1,34 +1,34 @@
 echo -e\ "\e[33m Downloading the NodeJS\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/robodhop.log
 
 echo  -e "e\[33m Installing the NodeJS\e[0m"
-yum install nodejs -y
+yum install nodejs -y &>>/tmp/robodhop.log
 
 echo -e "e\[33m Adding User Roboshop\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/robodhop.log
 
 
 rm -rf app
 echo -e "e\[33m Adding Directory\e[0m"
 mkdir /app
 echo -e "e\[33m Downloading catalogue file\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/robodhop.log
 cd /app
 echo -e "e\[33m extracting catalogue file\e[0m"
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>/tmp/robodhop.log
 
 cd /app
 echo -e "e\[33m installing npm\e[0m"
-npm install
+npm install &>>/tmp/robodhop.log
 echo -e "e\[33m copy catalogue service file\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/robodhop.log
 systemctl daemon-reload
 echo -e "e\[33m copy mongodb repo\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "e\[33m Installing Mongodb client\e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>/tmp/robodhop.log
 echo -e "e\[33m Loading schema\e[0m"
-mongo --host mongodb-dev.arshadev.online </app/schema/catalogue.js
+mongo --host mongodb-dev.arshadev.online </app/schema/catalogue.js &>>/tmp/robodhop.log
 echo -e "e\[33m Starting Catalogue\e[0m"
-systemctl enable catalogue
-systemctl restart catalogue
+systemctl enable catalogue &>>/tmp/robodhop.log
+systemctl restart catalogue &>>/tmp/robodhop.log

1b11e2381931c0c896cd27a3e3a9d81cbdb8ce83
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-26 23:47:29 +0530

    changed roboshop.conf file and mongodb.sh

--- redis.sh
+++ redis.sh
@@ -7,6 +7,6 @@ echo -e "\e[33m Installing Redis\e[0m"
 yum install redis -y &>>/tmp/robodhop.log
 sep -i 's/127.0.0.0/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>>/tmp/robodhop.log
 
-echo -e "\e[33m Starting Redis Server\e[0m"
+echo -e "\e[33m Start Redis Server\e[0m"
 systemctl enable redis &>>/tmp/robodhop.log
 systemctl restart redis &>>/tmp/robodhop.log

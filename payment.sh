2694127148dfcf0ed35f712c08cefabedc4cdecd
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-26 19:49:32 +0530

    small changes in echo

--- payment.sh
+++ payment.sh
@@ -3,21 +3,21 @@
 yum install python36 gcc python3-devel -y
 
 
-echo -e "e\[33m Adding user\e[0m"
+echo -e "\e[33m Adding user\e[0m"
 useradd roboshop &>>/tmp/robodhop.log
 
 
 rm -rf app
-echo -e "e\[33m Adding Directory\e[0m"
+echo -e "\e[33m Adding Directory\e[0m"
 mkdir /app
-echo -e "e\[33m Downloading Payment file\e[0m"
+echo -e "\e[33m Downloading Payment file\e[0m"
 curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/robodhop.log
 cd /app
-echo -e "e\[33m extracting payment file\e[0m"
+echo -e "\e[33m extracting payment file\e[0m"
 unzip /tmp/payment.zip &>>/tmp/robodhop.log
 
 
-echo -e "e\[33m Starting payment server\e[0m"
+echo -e "\e[33m Starting payment server\e[0m"
 systemctl daemon-reload  &>>/tmp/robodhop.log
 systemctl enable payment  &>>/tmp/robodhop.log
 systemctl restart payment  &>>/tmp/robodhop.log

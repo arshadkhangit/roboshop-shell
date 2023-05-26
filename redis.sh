698aab63ebfcc957b7c579ba649704e076c33ed0
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-26 19:39:14 +0530

    dispatch script

--- redis.sh
+++ redis.sh
@@ -1,12 +1,12 @@
 echo -e "\e[33m installing Redis repo\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/robodhop.log
 
 echo -e "\e[33m Enabling Redis version\e[0m"
-yum module enable redis:remi-6.2 -y
+yum module enable redis:remi-6.2 -y  &>>/tmp/robodhop.log
 echo -e "\e[33m Installing Redis\e[0m"
-yum install redis -y
-sep -i 's/127.0.0.0/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf
+yum install redis -y &>>/tmp/robodhop.log
+sep -i 's/127.0.0.0/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>>/tmp/robodhop.log
 
 echo -e "\e[33m Starting Redis Server\e[0m"
-systemctl enable redis
-systemctl restart redis
+systemctl enable redis &>>/tmp/robodhop.log
+systemctl restart redis &>>/tmp/robodhop.log

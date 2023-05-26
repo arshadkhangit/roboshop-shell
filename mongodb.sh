e6e5e592a13f92d5c321e9a5477e628b6f1f1121
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-26 23:40:58 +0530

    changed roboshop.conf file and mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,10 @@
-echo -e "\e[33mcop mongodb repo\e[0m"
+echo -e "\e[33mcopy mongodb repo\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/robodhop.log
 echo -e "\e[33minstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/robodhop.log
 
-#modify the config file
-echo -e "\e[33mstarting mongodb server\e[0m"
+echo -e "\e[33m updating mongodb config\e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo -e "\e[33m starting mongodb server\e[0m"
 systemctl enable mongod &>>/tmp/robodhop.log
 systemctl restart mongod &>>/tmp/robodhop.log

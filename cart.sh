67535c4cddb8e982560f9e1f16b50c7719c81f50
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-26 19:02:00 +0530

    payment script

--- cart.sh
+++ cart.sh
@@ -22,6 +22,7 @@ echo -e "e\[33m installing npm\e[0m"
 npm install &>>/tmp/robodhop.log
 echo -e "e\[33m copy cart service file\e[0m"
 cp cart.service /etc/systemd/system/cart.service &>>/tmp/robodhop.log
+echo -e "e\[33m Starting cart server\e[0m"
 systemctl daemon-reload  &>>/tmp/robodhop.log
 systemctl enable cart  &>>/tmp/robodhop.log
 systemctl restart cart  &>>/tmp/robodhop.log

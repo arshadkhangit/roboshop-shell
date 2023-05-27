c36217d1d3e6e188b0a0e436dd377000f9736abe
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-27 13:06:20 +0530

    changed roboshop.conf file and mongodb.sh

--- frontend.sh
+++ frontend.sh
@@ -6,9 +6,12 @@ rm -rf /usr/share/nginx/html/* &>>/tmp/robodhop.log
 echo -e "\e[33mdownloading frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/robodhop.log
 
-echo -e "\e[33mextraacting frontend contend"
+echo -e "\e[33mextraacting frontend contend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/robodhop.log
+
+echo -e "\e[33m Copy roboshop.conf \e[0m"
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/robodhop.log
 
 echo -e "\e[33mstarting nginx server\e[0m"
 systemctl enable nginx &>>/tmp/robodhop.log

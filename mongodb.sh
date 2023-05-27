6bf27731068e81245a26e7ee62a160d7fcc13fe2
arshadkhangit <132884179+arshadkhangit@users.noreply.github.com>
2023-05-27 06:36:33 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 echo -e "\e[33mcopy mongodb repo\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/robodhop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/robodhop.log
 echo -e "\e[33minstalling mongodb server\e[0m"
-yum install mongodb-org -y &>>/tmp/robodhop.log
+yum install mongodb-org -y  &>>/tmp/robodhop.log
 
 echo -e "\e[33m updating mongodb config\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf

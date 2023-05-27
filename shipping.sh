0c343ff07672055a0c72195f646901ec97b6f6ac
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 00:50:21 +0530

    edits in shipping file

--- shipping.sh
+++ shipping.sh
@@ -21,13 +21,13 @@ mv target/shipping-1.0.jar shipping.jar &>>/tmp/robodhop.log
 
 echo -e "\e[33m Copy shipping service \e[0m"
 cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/robodhop.log
-
+systemctl daemon-reload &>>/tmp/robodhop.log
 echo -e "\e[33m Installing Mysql client \e[0m"
 yum install mysql -y &>>/tmp/robodhop.log
 echo -e "\e[33m Loading Schema \e[0m"
 mysql -h mysql-dev.arshadev.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/robodhop.log
 
 echo -e "\e[33m starting Shipping server \e[0m"
-systemctl daemon-reload &>>/tmp/robodhop.log
+
 systemctl enable shipping &>>/tmp/robodhop.log
 systemctl restart shipping &>>/tmp/robodhop.log

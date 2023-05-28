6c43c00d3639a83f50026816691bc83da5349a13
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 11:43:34 +0530

    edits in shipping file

--- cart.sh
+++ cart.sh
@@ -17,10 +17,10 @@ cd /app
 echo -e "\e[33m extracting cart file\e[0m"
 unzip /tmp/cart.zip &>>/tmp/robodhop.log
 
-cd /app
+
 echo -e "\e[33m installing npm\e[0m"
 npm install &>>/tmp/robodhop.log
-echo -e "\e[33m copy cart service file\e[0m"
+echo -e "\e[33m copy cart.service file\e[0m"
 cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/robodhop.log
 echo -e "\e[33m Starting cart server\e[0m"
 systemctl daemon-reload  &>>/tmp/robodhop.log

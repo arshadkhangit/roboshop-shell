8c8f5c61391fd14ee9c944d89362bf13e3faffe9
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 15:13:08 +0530

    edits in shipping file

--- redis.sh
+++ redis.sh
@@ -5,7 +5,9 @@ echo -e "\e[33m Enabling Redis version\e[0m"
 yum module enable redis:remi-6.2 -y  &>>/tmp/robodhop.log
 echo -e "\e[33m Installing Redis\e[0m"
 yum install redis -y &>>/tmp/robodhop.log
-sep -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf &>>/tmp/robodhop.log
+
+echo -e "\e[33m Updating redis listen address\e[0m"
+sep -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/robodhop.log
 
 echo -e "\e[33m Start Redis Server\e[0m"
 systemctl enable redis &>>/tmp/robodhop.log

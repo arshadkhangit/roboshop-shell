a17d6448104a287792cf13455173472280c7877c
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 19:32:09 +0530

    edits in shipping file

--- catalogue.sh
+++ catalogue.sh
@@ -1,40 +1,41 @@
 component=catalogue
 color="\e[35m"
 nocolor="\e[0m"
-
+log_file="/tmp/robodhop.log"
+app_path="/app"
 
 echo -e "${color} Configuring the NodeJS${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/robodhop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
 echo  -e "${color} Installing the NodeJS${nocolor}"
-yum install nodejs -y &>>/tmp/robodhop.log
+yum install nodejs -y &>>$log_file
 
 echo -e "${color} Adding User Roboshop${nocolor}"
-useradd roboshop &>>/tmp/robodhop.log
+useradd roboshop &>>$log_file
 
-rm -rf /app
+rm -rf ${app_path}
 echo -e "${color} Adding Directory${nocolor}"
-mkdir /app
+mkdir ${app_path}
 echo -e "${color} Downloading $component file${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/robodhop.log
-cd /app
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+cd ${app_path}
 echo -e "${color} extracting $component file${nocolor}"
-unzip /tmp/$component.zip &>>/tmp/robodhop.log
+unzip /tmp/$component.zip &>>$log_file
 
-cd /app
+cd ${app_path}
 echo -e "${color} installing npm${nocolor}"
-npm install &>>/tmp/robodhop.log
+npm install &>>$log_file
 echo -e "${color} copy $component service file${nocolor}"
-cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>/tmp/robodhop.log
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
 echo -e "${color} Starting $component${nocolor}"
-systemctl daemon-reload &>>/tmp/robodhop.log
-systemctl enable $component &>>/tmp/robodhop.log
-systemctl restart $component &>>/tmp/robodhop.log
+systemctl daemon-reload &>>$log_file
+systemctl enable $component &>>$log_file
+systemctl restart $component &>>$log_file
 echo -e "${color} copy mongodb repo${nocolor}"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/robodhop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>$log_file
 echo -e "${color} Installing Mongodb client${nocolor}"
-yum install mongodb-org-shell -y  &>>/tmp/robodhop.log
+yum install mongodb-org-shell -y  &>>$log_file
 echo -e "${color} Loading schema${nocolor}"
-mongo --host mongodb-dev.arshadev.online </app/schema/$component.js  &>>/tmp/robodhop.log
+mongo --host mongodb-dev.arshadev.online <${app_path}/schema/$component.js  &>>$log_file
 
 

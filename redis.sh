0d3d1d1bb9eee6da539ecd52ff2fdecfa6ea870d
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 17:16:51 +0530

    edits in shipping file

--- redis.sh
+++ redis.sh
@@ -11,4 +11,4 @@ sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/robo
 
 echo -e "\e[33m Start Redis Server\e[0m"
 systemctl enable redis &>>/tmp/robodhop.log
-systemctl start redis &>>/tmp/robodhop.log
+systemctl restart redis &>>/tmp/robodhop.log

4b8433a52ed779ce00ad6f4b8e5615b003f22d9a
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 13:14:36 +0530

    edits in shipping file

--- cart.sh
+++ cart.sh
@@ -1,29 +1,28 @@
 echo -e "\e[33m Downloading Node repo \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/robodhop.log
 echo -e "\e[33m Installing Node js\e[0m"
-yum install nodejs -y
+yum install nodejs -y  &>>/tmp/robodhop.log
 echo -e "\e[33m adding user \e[0m"
-useradd roboshop
+useradd roboshop  &>>/tmp/robodhop.log
 
 echo -e "\e[33m Creating a Directory\e[0m"
-rm -rf /app
-mkdir /app
+rm -rf /app  &>>/tmp/robodhop.log
+mkdir /app  &>>/tmp/robodhop.log
 echo -e "\e[33m Dowanloading cart file \e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app
-unzip /tmp/cart.zip
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip  &>>/tmp/robodhop.log
+cd /app  &>>/tmp/robodhop.log
+unzip /tmp/cart.zip  &>>/tmp/robodhop.log
 
 echo -e "\e[33m Installing application \e[0m"
-cd /app
-npm install
+cd /app  &>>/tmp/robodhop.log
+npm install  &>>/tmp/robodhop.log
 
 
 echo -e "\e[33m copy service file\e[0m"
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service  &>>/tmp/robodhop.log
 
 
 echo -e "\e[33m starting cart server \e[0m"
-systemctl daemon-reload
-
-systemctl enable cart
-systemctl start cart
+systemctl daemon-reload  &>>/tmp/robodhop.log
+systemctl enable cart  &>>/tmp/robodhop.log
+systemctl start cart  &>>/tmp/robodhop.log

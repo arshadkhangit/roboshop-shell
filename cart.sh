04f042a6f06c4745b906e9a16e835207b166ac7a
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 20:21:45 +0530

    common file updating

--- cart.sh
+++ cart.sh
@@ -1,29 +1,32 @@
-echo -e "\e[33m Downloading Node repo \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/robodhop.log
-echo -e "\e[33m Installing Node js\e[0m"
-yum install nodejs -y  &>>/tmp/robodhop.log
-echo -e "\e[33m adding user \e[0m"
-useradd roboshop  &>>/tmp/robodhop.log
+source common.sh
+component=cart
 
-echo -e "\e[33m Creating a Directory\e[0m"
-rm -rf /app  &>>/tmp/robodhop.log
-mkdir /app  &>>/tmp/robodhop.log
-echo -e "\e[33m Dowanloading cart file \e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip  &>>/tmp/robodhop.log
-cd /app  &>>/tmp/robodhop.log
-unzip /tmp/cart.zip  &>>/tmp/robodhop.log
+echo -e "${color} Downloading Node repo ${nocolor}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$log_file
+echo -e "${color} Installing Node js${nocolor}"
+yum install nodejs -y  &>>$log_file
+echo -e "${color} adding user ${nocolor}"
+useradd roboshop  &>>$log_file
 
-cd /app
-echo -e "\e[33m Installing application \e[0m"
+echo -e "${color} Creating a Directory${nocolor}"
+rm -rf ${app_path}  &>>$log_file
+mkdir ${app_path}  &>>$log_file
+echo -e "${color} Dowanloading $component file ${nocolor}"
+curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>$log_file
+cd ${app_path}  &>>$log_file
+unzip /tmp/$component.zip  &>>$log_file
 
-npm install  &>>/tmp/robodhop.log
+cd ${app_path}
+echo -e "${color} Installing application ${nocolor}"
+
+npm install  &>>$log_file
 
 
-echo -e "\e[33m copy service file\e[0m"
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service  &>>/tmp/robodhop.log
+echo -e "${color} copy service file${nocolor}"
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>$log_file
 
 
-echo -e "\e[33m starting cart server \e[0m"
-systemctl daemon-reload  &>>/tmp/robodhop.log
-systemctl enable cart  &>>/tmp/robodhop.log
-systemctl restart cart  &>>/tmp/robodhop.log
+echo -e "${color} starting $component server ${nocolor}"
+systemctl daemon-reload  &>>$log_file
+systemctl enable $component  &>>$log_file
+systemctl restart $component  &>>$log_file

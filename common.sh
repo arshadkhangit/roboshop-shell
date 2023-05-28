d6d51974f6d4e2b98a3694be9d3585a187d1618b
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 21:07:41 +0530

    node js & schema function setup

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 color="\e[32m"
-nocolor="\e[0m"
+nocolor="${nocolor}"
 log_file="/tmp/robodhop.log"
 app_path="/app"
 
@@ -42,5 +42,43 @@ mongo_schema_setup(){
   yum install mongodb-org-shell -y  &>>$log_file
 
   echo -e "${color} Load Schema ${nocolor}"
-  mongo --host mongodb-dev.arshadev.online.store </app/schema/$component.js  &>>$log_file
+  mongo --host mongodb-dev.arshadev.online.store <${app_path}/schema/$component.js  &>>$log_file
+}
+
+maven(){
+  echo -e "${color} Installing Maven ${nocolor}"
+  yum install maven -y &>>$log_file
+  echo -e "${color} Adding user ${nocolor}"
+  useradd roboshop &>>$log_file
+
+  echo -e "${color} Creating Directory ${nocolor}"
+  rm -rf ${app_path} &>>$log_file
+  mkdir ${app_path} &>>$log_file
+
+  echo -e "${color} Downloading $component file ${nocolor}"
+  curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+
+  echo -e "${color} Extracting $component file ${nocolor}"
+  cd ${app_path} &>>$log_file &>>$log_file
+  unzip /tmp/$component.zip &>>$log_file
+
+  echo -e "${color} Cleaning package ${nocolor}"
+  mvn clean package &>>$log_file
+  mv target/$component-1.0.jar $component.jar &>>$log_file
+  echo -e "${color} Installing Mysql client ${nocolor}"
+  yum install mysql -y &>>$log_file
+
+  echo -e "${color} Loading Schema ${nocolor}"
+  mysql -h mysql-dev.arshadev.online -uroot -pRoboShop@1 < ${app_path}/schema/$component.sql &>>$log_file
+
+
+  echo -e "${color} Copy $component service ${nocolor}"
+  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
+
+
+
+  echo -e "${color} starting $component server ${nocolor}"
+  systemctl daemon-reload &>>$log_file
+  systemctl enable $component &>>$log_file
+  systemctl restart $component &>>$log_file
 }
--- shipping.sh
+++ shipping.sh
@@ -1,36 +1,4 @@
-echo -e "\e[33m Installing Maven \e[0m"
-yum install maven -y &>>/tmp/robodhop.log
-echo -e "\e[33m Adding user \e[0m"
-useradd roboshop &>>/tmp/robodhop.log
+source common.sh
+component=shipping
 
-echo -e "\e[33m Creating Directory \e[0m"
-rm -rf /app &>>/tmp/robodhop.log
-mkdir /app &>>/tmp/robodhop.log
-
-echo -e "\e[33m Downloading Shipping file \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/robodhop.log
-
-echo -e "\e[33m Extracting Shipping file \e[0m"
-cd /app &>>/tmp/robodhop.log &>>/tmp/robodhop.log
-unzip /tmp/shipping.zip &>>/tmp/robodhop.log
-
-echo -e "\e[33m Cleaning package \e[0m"
-mvn clean package &>>/tmp/robodhop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/robodhop.log
-echo -e "\e[33m Installing Mysql client \e[0m"
-yum install mysql -y &>>/tmp/robodhop.log
-
-echo -e "\e[33m Loading Schema \e[0m"
-mysql -h mysql-dev.arshadev.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/robodhop.log
-
-
-echo -e "\e[33m Copy shipping service \e[0m"
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/robodhop.log
-
-
-
-
-echo -e "\e[33m starting Shipping server \e[0m"
-systemctl daemon-reload &>>/tmp/robodhop.log
-systemctl enable shipping &>>/tmp/robodhop.log
-systemctl restart shipping &>>/tmp/robodhop.log
+maven

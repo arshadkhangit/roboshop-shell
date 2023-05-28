591e04e08bbc59be384d7a8a96bb4e93488ff6cd
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 20:36:18 +0530

    common file updating

--- cart.sh
+++ cart.sh
@@ -1,32 +1,4 @@
 source common.sh
 component=cart
 
-echo -e "${color} Downloading Node repo ${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$log_file
-echo -e "${color} Installing Node js${nocolor}"
-yum install nodejs -y  &>>$log_file
-echo -e "${color} adding user ${nocolor}"
-useradd roboshop  &>>$log_file
-
-echo -e "${color} Creating a Directory${nocolor}"
-rm -rf ${app_path}  &>>$log_file
-mkdir ${app_path}  &>>$log_file
-echo -e "${color} Dowanloading $component file ${nocolor}"
-curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>$log_file
-cd ${app_path}  &>>$log_file
-unzip /tmp/$component.zip  &>>$log_file
-
-cd ${app_path}
-echo -e "${color} Installing application ${nocolor}"
-
-npm install  &>>$log_file
-
-
-echo -e "${color} copy service file${nocolor}"
-cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>$log_file
-
-
-echo -e "${color} starting $component server ${nocolor}"
-systemctl daemon-reload  &>>$log_file
-systemctl enable $component  &>>$log_file
-systemctl restart $component  &>>$log_file
+nodejs
--- common.sh
+++ common.sh
@@ -2,3 +2,34 @@ color="\e[32m"
 nocolor="\e[0m"
 log_file="/tmp/robodhop.log"
 app_path="/app"
+
+
+nodejs() {
+  echo -e "${color} Configuring the NodeJS${nocolor}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+
+  echo  -e "${color} Installing the NodeJS${nocolor}"
+  yum install nodejs -y &>>$log_file
+
+  echo -e "${color} Adding User Roboshop${nocolor}"
+  useradd roboshop &>>$log_file
+
+  rm -rf ${app_path}
+  echo -e "${color} Adding Directory${nocolor}"
+  mkdir ${app_path}
+  echo -e "${color} Downloading $component file${nocolor}"
+  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+  cd ${app_path}
+  echo -e "${color} extracting $component file${nocolor}"
+  unzip /tmp/$component.zip &>>$log_file
+
+  cd ${app_path}
+  echo -e "${color} installing npm${nocolor}"
+  npm install &>>$log_file
+  echo -e "${color} copy $component service file${nocolor}"
+  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
+  echo -e "${color} Starting $component${nocolor}"
+  systemctl daemon-reload &>>$log_file
+  systemctl enable $component &>>$log_file
+  systemctl restart $component &>>$log_file
+}

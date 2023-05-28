d38282825e3074901cfff0c0176e20e1dc4c0e3c
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-05-28 12:07:36 +0530

    edits in shipping file

--- mysql.sh
+++ mysql.sh
@@ -10,10 +10,8 @@ yum install mysql-community-server -y &>>/tmp/robodhop.log
 
 echo -e "\e[33m mysql server starting \e[0m"
 systemctl enable mysqld &>>/tmp/robodhop.log
-systemctl start mysqld &>>/tmp/robodhop.log
+systemctl restart mysqld &>>/tmp/robodhop.log
 
 echo -e "\e[33m seting up  passwd \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/robodhop.log
 
-echo -e "\e[33m cecking password \e[0m"
-mysql -uroot -pRoboShop@1 &>>/tmp/robodhop.log

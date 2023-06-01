ad941069c45776c2fdb2bb7080417f441269d767
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-02 00:17:42 +0530

    exit status updates color code

--- common.sh
+++ common.sh
@@ -6,33 +6,57 @@ app_path="/app"
 app_presetup(){
   echo -e "${color} Adding User Roboshop${nocolor}"
   useradd roboshop &>>$log_file
-  echo $?
-
+  if [ $? -eq o ]; then
+    echo success
+  else
+    echo failure
+  fi
   echo -e "${color} Creating Directory ${nocolor}"
   rm -rf ${app_path} &>>$log_file
   mkdir ${app_path} &>>$log_file
-  echo $?
+   if [ $? -eq o ]; then
+      echo success
+   else
+      echo failure
+   fi
 
   echo -e "${color} Downloading $component file${nocolor}"
   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
   cd ${app_path}
-  echo $?
+   if [ $? -eq o ]; then
+      echo success
+   else
+      echo failure
+   fi
 
   echo -e "${color} extracting $component file${nocolor}"
   cd ${app_path}
   unzip /tmp/$component.zip &>>$log_file
-  echo $?
+   if [ $? -eq o ]; then
+      echo success
+   else
+      echo failure
+   fi
 }
 
 systemd_setup(){
   echo -e "${color} copy $component service file${nocolor}"
   cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
-  echo $?
+   if [ $? -eq o ]; then
+      echo success
+   else
+      echo failure
+   fi
   echo -e "${color} Starting $component${nocolor}"
   systemctl daemon-reload &>>$log_file
   systemctl enable $component &>>$log_file
   systemctl restart $component &>>$log_file
-  echo $?
+
+   if [ $? -eq o ]; then
+      echo success
+   else
+      echo failure
+   fi
 }
 
 
@@ -87,14 +111,22 @@ maven(){
 python(){
   echo -e "${color} Installing Python36 ${nocolor}"
   yum install python36 gcc python3-devel -y &>>$log_file
-  echo $?
+   if [ $? -eq o ]; then
+      echo success
+   else
+      echo failure
+   fi
 
   app_presetup
 
   echo -e "${color} Downloading and Installing Dependencies${nocolor}"
   cd ${app_path} &>>$log_file
   pip3.6 install -r requirements.txt &>>$log_file
-  echo $?
+   if [ $? -eq o ]; then
+      echo success
+   else
+      echo failure
+   fi
 
   systemd_setup
 }

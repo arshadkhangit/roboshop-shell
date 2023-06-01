311558a4aa9ac905d51b9513203bbd813d729065
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-02 00:28:58 +0530

    id check

--- common.sh
+++ common.sh
@@ -5,8 +5,10 @@ app_path="/app"
 
 app_presetup(){
   echo -e "${color} Adding User Roboshop${nocolor}"
-  useradd roboshop &>>$log_file
-  id robodhop
+  id robodhop &>>$log_file
+  if [$? -eq 1 ]; then
+    useradd roboshop &>>$log_file
+  fi
   if [ $? -eq 0 ]; then
     echo success
   else

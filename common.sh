d83ac8ec600678c5b7153fbd731bea51fb1cf786
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-02 16:39:40 +0530

    placing stst check

--- common.sh
+++ common.sh
@@ -80,7 +80,7 @@ mongo_schema_setup() {
   yum install mongodb-org-shell -y  &>>$log_file
   stat_check $?
   echo -e "${color} Load Schema ${nocolor}"
-  mongo --host mongodb-dev.arshadev.online.store <${app_path}/schema/$component.js  &>>$log_file
+  mongo --host mongodb-dev.arshadev.online <${app_path}/schema/$component.js  &>>$log_file
   stat_check $?
 }
 

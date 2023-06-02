c07c83056e5a43053d96ab72a2bec1f50c4ce144
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-02 19:35:11 +0530

    catalogue app

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,10 @@
 source common.sh
 echo -e "${color} copy mongodb repo${nocolor}"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/robodhop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
 stat_check $?
 
 echo -e "${color} installing mongodb server${nocolor}"
-yum install mongodb-org -y  &>>/tmp/robodhop.log
+yum install mongodb-org -y  &>>/tmp/roboshop.log
 stat_check $?
 
 echo -e "${color} updating mongodb config${nocolor}"
@@ -12,6 +12,6 @@ sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat_check $?
 
 echo -e "${color} starting mongodb server${nocolor}"
-systemctl enable mongod &>>/tmp/robodhop.log
-systemctl restart mongod &>>/tmp/robodhop.log
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
 stat_check $?
